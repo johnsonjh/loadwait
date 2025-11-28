34ff51a94d6287e147235e591d0af77235253161
Jeffrey H. Johnson <johnsonjh.dev@gmail.com>
2025-11-28 16:55:45 -0500

    Update email
    Signed-off-by: Jeffrey H. Johnson <johnsonjh.dev@gmail.com>

--- loadwait.c
+++ loadwait.c
@@ -1,7 +1,7 @@
 /*
  * SPDX-License-Identifier: MIT-0
  *
- * Copyright 2022 Jeffrey H. Johnson <trnsz@pobox.com>
+ * Copyright 2022 Jeffrey H. Johnson <johnsonjh.dev@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
